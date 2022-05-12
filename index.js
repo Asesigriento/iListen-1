b48fbe0949ae5bc52bee7c446ba694df4fb9dcd8
Daniel Cardozo <villa5041@gmail.com>
2022-05-12 11:36:12 +0200

    -2

--- index.js
+++ index.js
@@ -1,123 +1,28 @@
-const mysql = require('mysql');
-
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const { request } = require('http');
-const Growl = require('node-notifier').Growl;
-var notifier= new Growl({
-    name : 'Growl Name Used',
-    host: 'localhost',
-    port : 23053
-})
-const conexion = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'users'
-});
-conexion.connect();
-var app = express();
 
+var app = express();
 app.use(session({
     secret : 'secret',
     resave : true,
     saveUninitialized : true
 }));
+
+
+//Middleware que te permite cargar archivos de imagenes, musica, css etc.
 app.use(express.static(__dirname + '/public/'));
 app.use(express.static(__dirname + '/controllers/'));
 app.use(express.static(__dirname +'public/img'));
 app.use(express.static(__dirname +'public/music'));
-app.use(express.static(__dirname + '/routes/'));
+app.use(express.static('controllers'));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
-app.use(express.static('controllers'));
 
+//Importación de un módulo para obtener las rutas
 const routes = require('./routes/routes.js');
 app.use(routes);
 
-app.post('/reg',async(req,res)=>{
-    let nombre_register = req.body.nombre_register;
-    let pass_register = req.body.pass_register; 
-
-if(nombre_register && pass_register){
-    /*conexion.query('INSERT INTO usuarios (username,password) VALUES (?,?)',[nombre_register,pass_register],function(error,fields){
-        if(error) throw error;
-
-        res.redirect('/');
-    });*/
-    conexion.query('SELECT username from usuarios'),function(error,resultados){
-        var c = "";
-        resultados.forEach(element => {
-            if(error){
-                throw error;
-            }
-            if(element == nombre_register){
-                if(error){
-                    throw error;
-                }
-                c="hecho"; 
-            }
-        });
-        if(c == "hecho"){
-            conexion.query('INSERT INTO usuarios (username,password) VALUES (?,?)',[nombre_register,pass_register],function(error,fields){
-                if(error) throw error;
-            });
-        }
-        conexion.end();
-    }
-    res.redirect('/');
-}
-else{
-    //res.send('error :(');
-    //res.end();
-    notifier.notify({
-        title: 'Asies',
-        message : 'Holaa',
-        icon: undefined,
-        wait: false 
-    },
-    function(error,response){
-        console.log(response);
-    });
-    conexion.end();
-}
-});
-
-app.post('/login',async(req,res)=>{
-    let nombre_login = req.body.nombre_login;
-    let pass_login = req.body.pass_login; 
-    if (nombre_login && pass_login) {
-	    conexion.query('SELECT * FROM usuarios WHERE username = ? AND password = ?', [nombre_login, pass_login], function(error, results,fields) {
-	    if (error) {
-            throw error;
-        }
-    	if (results.length > 0) {
-            // Authenticate the user
-            req.session.loggedin = true;
-            req.session.nombre_login = nombre_login;
-            // Redirect to home page
-            app.use('/home',routes);
-        }
-        else {
-		res.send('Incorrect Username and/or Password!');
-		}		
-    });	
-    }else{
-        //res.send('error :(');
-        //res.end();
-        notifier.notify({
-            title: 'Asies',
-            message : 'Holaa',
-            icon: undefined,
-            wait: false 
-        },
-        function(error,response){
-            console.log(response);
-        });
-        
-    }
-    conexion.end();
-});
 app.listen(3000);
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,92 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+const mysql = require('mysql');
+//Conexión a la BBDD
+const conexion = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'users'
+});
+conexion.connect();
 
+//Rutas
 router.get('/', function(req,res){
     res.sendFile(path.join(__dirname+'/../views/index.html'));
 });
-
 router.get('/home',function(req,res){
-    res.sendFile(path.join(+__dirname + '/../views/principal.html'));
+    res.sendFile(path.join(__dirname + '/../views/principal.html'));
 });
 
+
+//Registro de usuario
+router.post('/reg',async(req,res)=>{
+    let nombre_register = req.body.nombre_register;
+    let pass_register = req.body.pass_register; 
+
+if(nombre_register && pass_register){
+    /*conexion.query('INSERT INTO usuarios (username,password) VALUES (?,?)',[nombre_register,pass_register],function(error,fields){
+        if(error) throw error;
+
+        res.redirect('/');
+    });*/
+    conexion.query('SELECT username from usuarios'),function(error,resultados){
+        var c = "";
+        resultados.forEach(element => {
+            if(error){
+                throw error;
+            }
+            if(element == nombre_register){
+                if(error){
+                    throw error;
+                }
+                c="hecho"; 
+            }
+        });
+        if(c == "hecho"){
+            conexion.query('INSERT INTO usuarios (username,password) VALUES (?,?)',[nombre_register,pass_register],function(error,fields){
+                if(error) throw error;
+            });
+        }
+        conexion.end();
+    }
+    //Lleva a la página principal de la web
+    res.redirect('/');
+}
+else{
+    res.send('error :(');
+    res.end();
+    conexion.end();
+}
+});
+
+
+//Login de usuario
+router.post('/login',async(req,res)=>{
+    let nombre_login = req.body.nombre_login;
+    let pass_login = req.body.pass_login; 
+    if (nombre_login && pass_login) {
+	    conexion.query('SELECT * FROM usuarios WHERE username = ? AND password = ?', [nombre_login, pass_login], function(error, results,fields) {
+	    if (error) {
+            throw error;
+        }
+    	if (results.length > 0) {
+            // Autentica el usuario
+            req.session.loggedin = true;
+            req.session.nombre_login = nombre_login;
+            // Redirecciona la pagina principal del usuario
+            res.redirect('/home')
+        }
+        else {
+		res.send('Incorrect Username and/or Password!');
+		}		
+    });	
+    }else{
+        res.send('error :(');
+        res.end();
+    }
+    conexion.end();
+});
+//Exportación del módulo
 module.exports = router;
