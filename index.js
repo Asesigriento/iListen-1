c21d06e7b8f6922f6a02df17bd07635ac2b6fb79
Daniel Cardozo <villa5041@gmail.com>
2022-05-19 12:08:31 +0200

    otra prueba

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ app.use(express.static('controllers'));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/views/index.html'));
-});
+
 //Importación de un módulo para obtener las rutas
 const routes = require('./routes/routes.js');
 app.use(routes);
