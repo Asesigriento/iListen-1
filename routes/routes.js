b5d79ab74129a94c4524d5434f1e8b81f86c5311
Daniel Cardozo <villa5041@gmail.com>
2022-05-24 10:32:08 +0200

    Introduciendo usuario

--- routes/routes.js
+++ routes/routes.js
@@ -11,11 +11,7 @@ const pool = new Pool({
 });
 router.get('/db', async (req, res) => {
     try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.sendFile(path.join(__dirname + '/../views/principal.html'));
-      client.release();
+      
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
@@ -41,45 +37,24 @@ router.get('/home',function(req,res){
 
 
 //Registro de usuario
-/*router.post('/reg',async(req,res)=>{
+router.post('/reg',async(req,res)=>{
     let nombre_register = req.body.nombre_register;
     let pass_register = req.body.pass_register; 
 
+    try{
 if(nombre_register && pass_register){
-    conexion.query('INSERT INTO usuarios (username,password) VALUES (?,?)',[nombre_register,pass_register],function(error,fields){
-        if(error) throw error;
 
+    const client = await pool.connect();
+      const result = await client.query('INSERT INTO usuarios (nombre,password) VALUES(?,?)',[nombre_register,pass_register]);
+      client.release();
         res.redirect('/');
-    });*/
-    /*conexion.query('SELECT username from usuarios'),function(error,resultados){
-        var c = "";
-        resultados.forEach(element => {
-            if(error){
-                throw error;
-            }
-            if(element == nombre_register){
-                if(error){
-                    throw error;
-                }
-                c="hecho"; 
-            }
-        });
-        if(c == "hecho"){
-            conexion.query('INSERT INTO usuarios (username,password) VALUES (?,?)',[nombre_register,pass_register],function(error,fields){
-                if(error) throw error;
-            });
-        }
-        conexion.end();*/
-   /* }
+    }
     //Lleva a la página principal de la web
     res.redirect('/');
-}
-else{
-    res.send('error :(');
-    res.end();
-    conexion.end();
-}
-});*/
+    }
+catch(error){console.log(error);}
+
+});
 
 
 
