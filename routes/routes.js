a7befc14aac425f6e432da97aee786df73ecced2
Daniel Cardozo <villa5041@gmail.com>
2022-05-24 09:13:56 +0200

    Conectando a la BBDD

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,19 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 const mysql = require('mysql');
+router.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.sendFile(path.join(__dirname + '/../views/principal.html'));
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 //Conexión a la BBDD
 /*const conexion = mysql.createConnection({
     host : 'localhost',
