bbc9052fdb0ca11ec4ab5ba1716f180ae96ee73d
Daniel Cardozo <villa5041@gmail.com>
2022-05-25 10:04:24 +0200

    poniendo ; a la query

--- routes/routes.js
+++ routes/routes.js
@@ -40,7 +40,7 @@ router.post('/reg',urlencodedParser,async(req,res)=>{
 if(nombre_register && pass_register){
     try{
         const client = await pool.connect();
-        const result = await client.query("INSERT INTO usuarios (nombre,password) VALUES(?,?)",[nombre_register,pass_register],(err,rest)=>{
+        const result = await client.query("INSERT INTO usuarios (nombre,password) VALUES(?,?);",[nombre_register,pass_register],(err,rest)=>{
            if(err){
                console.log("No se ha introducido nada");
                console.log(err);
