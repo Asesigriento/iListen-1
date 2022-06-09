8566637ae3552722e2066cf10f672ad2527b9986
Daniel Cardozo <villa5041@gmail.com>
2022-06-09 09:23:28 +0200

    funciona

--- controllers/principal.js
+++ controllers/principal.js
@@ -212,7 +212,7 @@ canciones.forEach(element => {
     const {id,titulocancion,portada} = element;
     let tarjeta= document.createElement('a');
     tarjeta.classList.add('tarjeta');
-    tarjeta.href=`"${titulocancion}"`;
+    tarjeta.href='#' + id;
     tarjeta.innerHTML = `
              <img src="${portada}" alt="">
              <div class="contenido">
